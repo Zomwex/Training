673e31816ea213d164250a83f4d2fe29c8b03559
Zomwex <42135592+Zomwex@users.noreply.github.com>
2020-05-11 08:28:51 +0300

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -2,3 +2,9 @@ name = Andrey
 height = 2
 weight = 20
 bmi height / (weight ** 2)
+print("индекс массы тела: " + str(bmi))
+
+if bmi < 25:
+  print("У " + name + " нетлишнего веса")
+else:
+  print("У " + name + " есть лишний вес")
